672f2461b0b66b6510593df09ebdf1eee630c573
Vladislav <vladozdeveloper@gmail.com>
2022-04-18 23:52:59 +0300

    connected Route and Routes without BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import { Route, Routes} from "react-router";
+import { Home } from './pages/Home/Home';
+import { MonthStatistics } from './pages/MonthStatistics/MonthStatistics';
 
 function App() {
   return (
-    <div className="App"> Привет </div>
+    <div className="App"> 
+      <Routes>
+        <Route path ="/" element={<Home/>} />
+        <Route path="/month-statistics" element={<MonthStatistics/>} />
+      </Routes>
+    </div>
   );
 }
 
