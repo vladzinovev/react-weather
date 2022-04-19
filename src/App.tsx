abe34110312d63831117df82fd89dfcb9661d05c
Vladislav <vladozdeveloper@gmail.com>
2022-04-19 15:58:38 +0300

    created block header

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Route, Routes} from "react-router";
 import { Home } from './pages/Home/Home';
 import { MonthStatistics } from './pages/MonthStatistics/MonthStatistics';
+import { Header } from './shared/Header/Header';
 
 function App() {
   return (
     <div className="App"> 
+      <Header/>
       <Routes>
         <Route path ="/" element={<Home/>} />
         <Route path="/month-statistics" element={<MonthStatistics/>} />
