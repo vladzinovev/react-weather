a4e769a5b7c3b914535f387d321d226c7ac70287
Vladislav <vladozdeveloper@gmail.com>
2022-04-19 23:38:14 +0300

    created block today temperature

---
+++ src/pages/Home/compoments/ThisDay/ThisDay.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './ThisDay.module.scss';
+
+interface Props{
+
+}
+
+export const ThisDayInfo = (props: Props) =>{
+    return <div className={s.home}> Home </div>
+};
