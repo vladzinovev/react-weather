e04f5dfec03acec35ad5b0107423fc4ebac283d7
Vladislav <vladozdeveloper@gmail.com>
2022-04-21 13:10:39 +0300

    created files for blocks temerature weekend

---
+++ src/pages/Home/compoments/Days/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { GlobalSvgSelector } from '../../../../assets/icons/global/GlobalSvgSelector';
+import s from './Days.module.scss';
+
+interface Props{
+}
+export const Card = (props: Props) =>{
+    return (
+        <div className={s.card}>
+            
+        </div>
+    ); 
+};
---
+++ src/pages/Home/compoments/Days/Days.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { GlobalSvgSelector } from '../../../../assets/icons/global/GlobalSvgSelector';
+import s from './Days.module.scss';
+
+interface Props{
+}
+export const Days = (props: Props) =>{
+    return (
+        <div className={s.days}>
+            
+        </div>
+    ); 
+};
