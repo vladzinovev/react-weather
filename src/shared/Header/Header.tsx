694d56bfca9a9d8ec90c6ffef23b8560ec485e8f
Vladislav <vladozdeveloper@gmail.com>
2022-04-22 14:33:39 +0300

    finished created root css variables and added new variables to all files

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -18,14 +18,18 @@ export const Header = (props: Props) =>{
     const colorStyles = {
         control: (styles:any)=>({
             ...styles,
-            backgroundColor: 'rgba(71,147,255,0.2)',
+            backgroundColor: 0 ? '#4f4f4f' :'rgba(71,147,255,0.2)',
             width: '194px',
             height: '37px',
             border: 'none',
             borderRadius: '10px',
             zIndex: '100',
-        })
-    }
+        }),
+    singleValue: (styles: any) => ({
+        ...styles,
+        color: 1 ? '#fff' : '#000',
+        }),
+    };
 
     return (
         <header className={s.header}> 
