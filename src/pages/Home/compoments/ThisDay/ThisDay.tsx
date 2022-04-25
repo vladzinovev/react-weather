934c58850687362817f63e4e5bbd380f139e6973
Vladislav <vladozdeveloper@gmail.com>
2022-04-25 13:44:34 +0300

    fixed a bug

--- src/pages/Home/compoments/ThisDay/ThisDay.tsx
+++ src/pages/Home/compoments/ThisDay/ThisDay.tsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React from 'react';
 import { GlobalSvgSelector } from '../../../../assets/icons/global/GlobalSvgSelector';
-import s from './ThisDay.module.scss';
 import { Weather } from '../../../../store/types/types';
+import s from './ThisDay.module.scss';
 
-interface Props{
+interface Props {
+  weather: Weather;
 }
-export const ThisDay = (props: Props) =>{
-    return (
-        <div className={s.this__day}>
-            <div className={s.top__block}>
-                <div className={s.top__block_wrapper}>
-                    <div className={s.this__temp}>{Math.floor(weather.main.temp)}°</div>
-                    <div className={s.this__today}>Сегодня</div>
-                </div>
-                <GlobalSvgSelector id="sun"/>
-            </div>
-            <div className={s.bottom__block}>
-                <div className={s.this__time}>
-                    Время: <span>21:54</span>
-                </div>
-                <div className={s.this__city}>
-                    Город: <span>Санкт-Петербург</span> 
-                </div>
-            </div>
+
+export const ThisDay = ({ weather }: Props) => {
+  return (
+    <div className={s.this__day}>
+      <div className={s.top__block}>
+        <div className={s.top__block_wrapper}>
+          <div className={s.this__temp}>{Math.floor(weather.main.temp)}°</div>
+          <div className={s.this__day_name}>Сегодня</div>
         </div>
-    ); 
+        <GlobalSvgSelector id="sun" />
+      </div>
+      <div className={s.bottom__block}>
+        <div className={s.this__time}>
+          Время: <span>21:54</span>
+        </div>
+        <div className={s.this__city}>
+          Время: <span>Санкт-Петербург</span>
+        </div>
+      </div>
+    </div>
+  );
 };
